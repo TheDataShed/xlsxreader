c5a5506be705dcab5c5c6331a3822430c551cfad
Douglas Parsons <dglsparsons@users.noreply.github.com>
2020-08-21 12:36:15 +0100

    Handle worksheets via relationship ID rather than implicit names (#16)

--- sheets.go
+++ sheets.go
@@ -3,7 +3,7 @@ package xlsxreader
 import (
 	"archive/zip"
 	"encoding/xml"
-	"strconv"
+	"fmt"
 )
 
 // workbook is a struct representing the data we care about from the workbook.xml file.
@@ -13,8 +13,27 @@ type workbook struct {
 
 // sheet is a struct representing the sheet xml element.
 type sheet struct {
-	Name    string `xml:"name,attr,omitempty"`
-	SheetID int    `xml:"sheetId,attr,omitempty"`
+	Name           string `xml:"name,attr,omitempty"`
+	RelationshipID string `xml:"http://schemas.openxmlformats.org/officeDocument/2006/relationships id,attr,omitempty"`
+}
+
+// relationships is a struct representing the data we care about from the _rels/workboox.xml.rels file.
+type relationships struct {
+	Relationships []relationship `xml:"Relationship"`
+}
+
+type relationship struct {
+	ID     string `xml:"Id,attr,omitempty"`
+	Target string `xml:"Target,attr,omitempty"`
+}
+
+func getFileNameFromRelationships(rels []relationship, s sheet) (string, error) {
+	for _, rel := range rels {
+		if rel.ID == s.RelationshipID {
+			return "xl/" + rel.Target, nil
+		}
+	}
+	return "", fmt.Errorf("Unable to find file with relationship %s", s.RelationshipID)
 }
 
 // getWorksheets loads the workbook.xml file and extracts a list of worksheets, along
@@ -31,8 +50,23 @@ func getWorksheets(files []*zip.File) ([]string, *map[string]*zip.File, error) {
 		return nil, nil, err
 	}
 
-	var wb workbook
+	wb := workbook{}
 	err = xml.Unmarshal(data, &wb)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	relsFile, err := getFileForName(files, "xl/_rels/workbook.xml.rels")
+	if err != nil {
+		return nil, nil, err
+	}
+	relsData, err := readFile(relsFile)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rels := relationships{}
+	err = xml.Unmarshal(relsData, &rels)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -41,7 +75,10 @@ func getWorksheets(files []*zip.File) ([]string, *map[string]*zip.File, error) {
 	sheetNames := make([]string, len(wb.Sheets))
 
 	for i, sheet := range wb.Sheets {
-		sheetFilename := "xl/worksheets/sheet" + strconv.Itoa(sheet.SheetID) + ".xml"
+		sheetFilename, err := getFileNameFromRelationships(rels.Relationships, sheet)
+		if err != nil {
+			return nil, nil, err
+		}
 		sheetFile, err := getFileForName(files, sheetFilename)
 		if err != nil {
 			return nil, nil, err
